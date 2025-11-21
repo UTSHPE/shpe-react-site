a582df38f0a2a6dfbc7ff83a831f2f804745e5f3
Luigi Medrano <luigimedrano03@gmail.com>
2025-11-20 20:46:08 -0600

    updated home page

--- utshpe-react-site/tailwind.config.js
+++ utshpe-react-site/tailwind.config.js
@@ -3,19 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false,
   theme: {
-    extend: {},
-  },
-  plugins: [],
-
-  theme: {
-  extend: {
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
-      code: ["Fira Code", "monospace"],
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        code: ["Fira Code", "monospace"],
+      },
     },
   },
-}
-
-}
-
+  plugins: [],
+};
